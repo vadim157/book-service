09dbbfbcff49f6ea4115da1297cc0bd79cc96bb5
vadim157 <trofimets@inbox.ru>
2022-11-03 22:23:01 +0300

    commit

--- src/main/java/com/trofimets/springcloud/bookservice/init/ApplicationRunnerImpl.java
+++ src/main/java/com/trofimets/springcloud/bookservice/init/ApplicationRunnerImpl.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
